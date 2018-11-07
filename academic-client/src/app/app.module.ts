f41fc51e13ed65d36e53b061a8be8846022086a6
guitmello <guillemello@outlook.com>
2018-11-07 19:49:32 -0200

    Update package.json, added app-routing-module and loginForm

--- academic-client/src/app/app.module.ts
+++ academic-client/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './security/login/login.component';
+
+import { LoginService } from './security/login/login.service';
+
+import {
+  MatCardModule, 
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- academic-client/src/app/security/login/login.module.ts
+++ academic-client/src/app/security/login/login.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+
+import {
+  MatCardModule, 
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   declarations: [LoginComponent],
 })
