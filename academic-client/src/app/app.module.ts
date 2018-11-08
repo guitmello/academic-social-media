61d86bf92118d1cbe4ca7cbd40c30164c855f49b
Avanade <Avanade@WIN-NCCOJBMMAH.corp.avanade.org>
2018-11-07 22:00:10 -0200

    Project structure and Timeline-project created

--- academic-client/src/app/app.module.ts
+++ academic-client/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
-import { PostModule } from './post/post.module';
+import { FormsModule } from '@angular/forms';
+
+import { HeaderComponent } from '../components/header/header.component';
+import { TimelineProjectComponent } from './views/timeline-project/timeline-project.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    TimelineProjectComponent
   ],
   imports: [
     BrowserModule,
-    PostModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
