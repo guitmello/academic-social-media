41282a7c658b86efacf6e8b2e89bab811646f2e9
Avanade <Avanade@WIN-Y2A3CZ3UE7.corp.avanade.org>
2018-11-11 23:04:27 -0200

    create file userFunctions.js

---
+++ api/util/userFunctions.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Joi = require('joi')
+const Boom = require('boom')
+const bcrypt = require('bcrypt')
+const Users = require('../models/users')
+
+const validCreateUser = Joi.object({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    area: Joi.string().required(),
+    photo: Joi.string().required(),
+    gender: Joi.string().required(),
+    phone: Joi.string().required(),
+    cpf: Joi.string().required(),
+    birthDate: Joi.date().required(),
+})
+
+async function verifyCredentials(email, password) {
+    const user = await Users.findOne({ email: email })
+    if (user) {
+        const isValid = await bcrypt.compare(password, user.password)
+        return (isValid) ? user : Boom.badRequest('Usuário ou senha inválida!', false)
+    } else {
+        return Boom.badRequest('Usuário ou senha inválida!', false)
+    }
+}
+
+module.exports = {
+    validCreateUser: validCreateUser,
+    verifyCredentials: verifyCredentials
+}
