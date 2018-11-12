f7c5fbdabfdce63e20ad3f39430546a44ba48a62
alyfmendonca <alyfmendonca1@gmail.com>
2018-11-12 01:02:30 -0200

    logout button

--- academic-client/src/app/header/header.component.ts
+++ academic-client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../security/login/login.service'
 
 @Component({
   selector: 'app-header',
@@ -8,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   search = '';
 
-  constructor() { }
+  constructor(private loginService: LoginService,) { }
 
   ngOnInit() {
   }
 
   onSearch() {
     console.log(this.search);
+  }
+
+  logout(){
+    this.loginService.logout();
   }
 }
