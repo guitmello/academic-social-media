a5218eb76d08f2f345e9727920dc4e8e99170c2d
Vitor Stipanich <vitorstipanich@hotmail.com>
2018-11-11 23:28:33 -0200

    POST Login HTTP code fixed

--- api/routes/users.js
+++ api/routes/users.js
@@ -21,17 +21,16 @@ module.exports = [
                 const result = await verifyCredentials(email, password)
 
                 if (result.isBoom) {
-                    return result.output.payload.message
+                    return Boom.badRequest(result.output.payload.message)
                 }
 
                 const token = await createToken(result)
-
                 const { _id, name, createdAt, area, photo, gender, birthDate, cpf } = result
 
                 return { name, _id, email, createdAt, area, photo, gender, birthDate, cpf, token }
             } catch (err) {
                 // return Boom.internal(error)
-                const item = Logs.obterDadoRequest(request, request.auth.credentials.username)
+                const item = Logs.obterDadoRequest(req)
                 Logs.logError(item.path, { ...item, err })
                 return Boom.internal()
             }
